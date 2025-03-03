e7fb452ef2741dd6bed46e01a773131c3078d0c9
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-03-03 20:39:35 +0200

    Time: 43 ms (6.87%), Space: 22.9 MB (10.54%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
+        unordered_map<int, int> map;
+    
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.find(nums[i]) != map.end()) {
                 nums.erase(nums.begin() + i);
                 i--;
+            } else {
+                map.insert(make_pair(nums[i], 1));
             }
         }
 
