60b5bbdeb2f8ceb93eca3537b261e2c2b62fac70
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-03-06 15:28:39 +0200

    Time: 1299 ms (5.02%), Space: 378 MB (5.05%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int numCols = 10000;
+
+        vector<vector<char>> mat(numRows, vector<char>(numCols, '*'));
+
+        int idx = 0;
+        int col = 0;
+        while (idx < s.size()) {
+            for (int i = 0; i < numRows && idx < s.size(); i++) {
+                mat[i][col] = s[idx];
+                idx++;
+            }
+
+            for (int i = numRows - 2; i > 0 && idx < s.size(); i--) {
+                col++;
+                mat[i][col] = s[idx];
+                idx++;
+            }
+            col++;
+        }
+
+        string result = "";
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (mat[i][j] != '*') {
+                    result += mat[i][j];
+                }
+            }
+        }
+
+        return result;
+    }
+};
