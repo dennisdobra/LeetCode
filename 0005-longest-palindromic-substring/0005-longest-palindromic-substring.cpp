b3ab679013189c9e3cd6da66e7da6e7e71b80479
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-03-17 16:56:49 +0200

    Time: 90 ms (41.72%), Space: 9.3 MB (94.19%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int left, right;
+        bool isPalindrome;
+        for (int len = s.size(); len > 0; len--) {
+            for (int start = 0; start <= s.size() - len; start++) {
+                /* check if the current substring is a palindrome */
+                left = start;
+                right = start + len - 1;
+                isPalindrome = true;
+                while (left < right) {
+                    if (s[left] != s[right]) {
+                        isPalindrome = false;
+                        break;
+                    }
+
+                    left++;
+                    right--;
+                }
+
+                if (isPalindrome) {
+                    return s.substr(start, len);
+                }
+            }
+        }
+
+        return "";
+    }
+};
