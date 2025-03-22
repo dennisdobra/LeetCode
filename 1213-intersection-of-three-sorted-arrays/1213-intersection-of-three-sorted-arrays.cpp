b25395fc3d091ee8eb65a8afa256bc2949b60d7d
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-03-22 13:17:41 +0200

    Time: 35 ms (5.51%), Space: 119.7 MB (6.3%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.cpp
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     vector<int> arraysIntersection(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3) {
+        vector<int> tmp;
         vector<int> result;
 
+        // intersect first two
         for (int i = 0; i < arr1.size(); i++) {
-            if (BinarySearch(arr2, arr1[i]) && BinarySearch(arr3, arr1[i])) {
-                result.push_back(arr1[i]);
+            if (BinarySearch(arr2, arr1[i])) {
+                // arr1.erase(arr1.begin() + i, arr1.begin() + i + 1);
+                tmp.emplace_back(arr1[i]);
+            }
+        }
+
+        for (int i = 0; i < tmp.size(); i++) {
+            if (BinarySearch(arr3, tmp[i])) {
+                result.emplace_back(tmp[i]);
             }
         }
 
