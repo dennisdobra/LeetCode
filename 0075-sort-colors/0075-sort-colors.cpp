961faaeb0f78ba7f939873505f406e0a4b565c1e
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-03-24 15:15:22 +0200

    Time: 0 ms (100%), Space: 11.8 MB (8.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,16 +1,43 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // 0 - red
-        // 1 - white
-        // 2 - blue
-
-        for (int i = 0; i < nums.size() - 1; i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] > nums[j]) {
-                    swap(nums[i], nums[j]);
-                }
+        int left = 0, right = nums.size() - 1;
+        int reds = 0, whites = 0;
+        while (left < right) {
+            if (nums[left] == 0) {
+                reds++;
+            } else if (nums[left] == 1) {
+                whites++;
             }
+
+            if (nums[right] == 0) {
+                reds++;
+            } else if (nums[right] == 1) {
+                whites++;
+            }
+
+            left++;
+            right--;
+        }
+
+        if (nums.size() % 2 != 0) {
+            if (nums[left] == 0) {
+                reds++;
+            } else if (nums[left] == 1) {
+                whites++;
+            }
+        }
+        
+        for (int i = 0; i < reds; i++) {
+            nums[i] = 0;
+        }
+
+        for (int i = reds; i < whites + reds; i++) {
+            nums[i] = 1;
+        }
+
+        for (int i = reds + whites; i < nums.size(); i++) {
+            nums[i] = 2;
         }
     }
 };
