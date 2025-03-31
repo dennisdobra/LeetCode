0512955d4385954197763bf95bb89b56f52197b3
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-03-31 22:34:43 +0300

    Time: 0 ms (100%), Space: 89.4 MB (60.78%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
-        int answer = 0;
+        // int answer = 0;
 
-        vector<long> prefix = {nums[0]};
-        for (int i = 1; i < nums.size(); i++) {
-            prefix.push_back(nums[i] + prefix.back());
+        // vector<long> prefix = {nums[0]};
+        // for (int i = 1; i < nums.size(); i++) {
+        //     prefix.push_back(nums[i] + prefix.back());
+        // }
+
+        // for (int i = 0; i < nums.size() - 1; i++) {
+        //     if (prefix[i] >= prefix[nums.size() - 1] - prefix[i]) {
+        //         answer++;
+        //     }
+        // }
+
+        // return answer;
+
+        int answer = 0;
+        long leftSection = 0;
+        long totalSum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            totalSum += nums[i];
         }
 
         for (int i = 0; i < nums.size() - 1; i++) {
-            if (prefix[i] >= prefix[nums.size() - 1] - prefix[i]) {
+            leftSection += nums[i];
+
+            if (leftSection >= totalSum - leftSection) {
                 answer++;
             }
         }
