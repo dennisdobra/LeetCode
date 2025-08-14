2464c846e9093890c1b75bae0d2723d94acf0547
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-08-15 01:18:38 +0300

    Time: 0 ms (100%), Space: 10.9 MB (21.55%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int curr_alt = 0;
+
+        int max_alt = 0;
+        for (int i = 0; i < gain.size(); i++) {
+            curr_alt += gain[i];
+
+            if (curr_alt > max_alt) {
+                max_alt = curr_alt;
+            }
+        }
+
+        return max_alt;
+    }
+};
