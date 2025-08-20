dac4ae3e0a2ea50af1460dd288d43cce6e1da1ca
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-08-20 17:45:19 +0300

    Time: 9 ms (56.61%), Space: 11.8 MB (68.78%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int> myMap;
+
         int ans = 0;
-        
-        unordered_set<char> characters;
-        
         int left = 0;
         for (int right = 0; right < s.size(); right++) {
-            while (characters.contains(s[right])) {
-                characters.erase(s[left]);
-                left++;
+            /* check if we found a duplicate */
+            if (myMap[s[right]] > 0) {
+                /* set left to the index of the first occurence of the current letter */
+                left = max(left, myMap[s[right]]);
             }
 
             ans = max(ans, right - left + 1);
-            characters.insert(s[right]);
+            myMap[s[right]] = right + 1;
         }
-        
+
         return ans;
     }
 };
