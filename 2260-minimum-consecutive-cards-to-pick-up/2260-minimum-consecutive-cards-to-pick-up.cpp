9ed1e63f9c678d8fcd45c256c0f0fa1e7a8bf05b
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-08-20 14:19:15 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumCardPickup(vector<int>& cards) {
+        /* sliding window */
+
+        /* hash map */
+        unordered_map<int,int> myMap;
+
+        int ans = INT_MAX;
+        for (int i = 0; i < cards.size(); i++) {
+            if (myMap.find(cards[i]) != myMap.end()) {
+                ans = min(ans, i - myMap[cards[i]] + 1);
+            } else {
+                myMap[cards[i]] = i;
+            }
+        }
+
+        if (ans == INT_MAX) return -1;
+
+        return ans;
+    }
+};
