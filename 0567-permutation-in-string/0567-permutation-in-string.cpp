fa34d1761c45cf69df5877b19e6da00e28f44676
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-08-22 19:00:18 +0300

    Time: 521 ms (9.2%), Space: 58.2 MB (9.28%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        /*
         if (s2.size() < s1.size()) return false;
 
         sort(s1.begin(), s1.end());
 
-        // initial window
+        // build the initial window
         string curr = "";
         int k;
         for (k = 0; k < s1.size(); k++) {
@@ -34,5 +35,54 @@ public:
         }
 
         return false;
+        */
+
+        if (s2.size() < s1.size()) return false;
+        unordered_map<char,int> s1map;
+
+        for (char c : s1) {
+            s1map[c]++;
+        }
+
+        string window = "";
+        for (int k = 0; k < s1.size(); k++) {
+            window += s2[k];
+        }
+
+        unordered_map<char,int> s2map;
+        for (char c : window) {
+            s2map[c]++;
+        }
+
+        if (s1map == s2map) return true;
+
+        for (int i = s1.size(); i < s2.size(); i++) {
+            // slide the window
+            window += s2[i];
+            window.erase(0, 1);
+
+            s2map.clear();
+
+            // build the map for the current window
+            for (char c : window) {
+                s2map[c]++;
+            }
+
+            if (s1map == s2map) return true;
+        }
+
+        return false;
+    }
+
+    bool equal(unordered_map<char,int> map1, unordered_map<char,int> map2) {
+        if (map1.size() != map2.size()) return false;
+
+        for (auto& pair : map1) {
+            if (!map2.contains(pair.first) || pair.second != map2[pair.first]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 };
