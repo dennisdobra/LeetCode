e9a6dea2694539bd8712876fa374a224c230895f
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-08-24 23:39:45 +0300

    Time: 0 ms (100%), Space: 11.1 MB (72.55%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        // save a pointer to the head for the result
+        ListNode* result = head;
+
+        // set a pointer to the node at position left and one at left - 1
+        int count = 1;
+        ListNode* leftBound = head;
+        ListNode* prevLeft = nullptr;
+        while (count < left) {
+            prevLeft = leftBound;
+            leftBound = leftBound->next;
+
+            count++;
+        }
+
+        // prevLeft = the node before the one at position left
+        // leftBound = the node at position left
+
+
+        // reverse from left to right
+        ListNode* prevNode = nullptr;
+        ListNode* curr = leftBound;
+
+        while (count <= right) {
+            ListNode* nextNode = curr->next;
+
+            curr->next = prevNode;
+            prevNode = curr;
+            curr = nextNode;
+
+            count++;
+        }
+
+        // connect to the left side
+        if (prevLeft != nullptr) {
+            // maybe left is the first node
+            prevLeft->next = prevNode;
+        } else {
+            result = prevNode;
+        }
+
+        leftBound->next = curr;
+
+        return result;
+    }
+};
