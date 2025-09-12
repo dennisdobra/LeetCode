3405e04d9552f4067d50e89d2fe094dfcb8c8f4e
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-09-13 02:00:26 +0300

    Time: 0 ms (100%), Space: 12.9 MB (14.84%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,25 +12,53 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // base case -> both null => true
-        if (p == nullptr && q == nullptr) {
-            return true;
+
+        /* RECURSIVE */
+        // // base case -> both null => true
+        // if (p == nullptr && q == nullptr) {
+        //     return true;
+        // }
+
+        // // only one is null => false
+        // if (p == nullptr || q == nullptr) {
+        //     return false;
+        // }
+
+        // if (p->val != q->val) {
+        //     return false;
+        // }
+
+        // // value of nodes is the same => check if the subtrees are also the same
+        // bool left = isSameTree(p->left, q->left);
+        // bool right = isSameTree(p->right, q->right);
+
+        // // if both subtrees are the same, we return true 
+        // return left && right;
+
+        /* ITERATIVE */
+        stack<pair<TreeNode*, TreeNode*>> stack;
+        stack.push({p, q});
+
+        while (!stack.empty()) {
+            auto [node1, node2] = stack.top();
+            stack.pop();
+
+            if (node1 == nullptr && node2 == nullptr) {
+                continue;
+            }
+
+            if (node1 == nullptr || node2 == nullptr) {
+                return false;
+            }
+
+            if (node1->val != node2->val) {
+                return false;
+            }
+
+            stack.push({node1->left, node2->left});
+            stack.push({node1->right, node2->right});
         }
 
-        // only one is null => false
-        if (p == nullptr || q == nullptr) {
-            return false;
-        }
-
-        if (p->val != q->val) {
-            return false;
-        }
-
-        // value of nodes is the same => check if the subtrees are also the same
-        bool left = isSameTree(p->left, q->left);
-        bool right = isSameTree(p->right, q->right);
-
-        // if both subtrees are the same, we return true 
-        return left && right;
+        return true;
     }
 };
