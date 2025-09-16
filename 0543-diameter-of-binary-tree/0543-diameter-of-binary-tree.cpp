d8fea46ce18e0c73254e9b7b2d31976375d870a5
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-09-16 13:50:59 +0300

    Time: 269 ms (14.9%), Space: 23.8 MB (42.99%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -13,22 +13,29 @@ class Solution {
 private:
     int diameter;
 
-    int longestPath(TreeNode* node) {
-        if (node == nullptr) return -1;
+    int maxDepth(TreeNode* node) {
+        if (node == nullptr) return 0;
 
-        // recursively find the longest path in both left and right children
-        int leftPath = longestPath(node->left);
-        int rightPath = longestPath(node->right);
-
-        diameter = max(diameter, leftPath + rightPath + 2);
-
-        return max(leftPath, rightPath) + 1;
+        int left = maxDepth(node->left);
+        int right = maxDepth(node->right);
+        return 1 + max(left, right);
     }
 
+    void dfs(TreeNode* root) {
+        if (root == nullptr) return;
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        diameter = max(diameter, leftDepth + rightDepth);
+
+        dfs(root->left);
+        dfs(root->right);
+    }
 public:
     int diameterOfBinaryTree(TreeNode* root) {
         diameter = 0;
-        longestPath(root);
+        dfs(root);
         return diameter;
-    }
+    }    
 };
