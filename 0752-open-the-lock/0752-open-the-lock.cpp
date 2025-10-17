0f74c126556e7fa00502c11a766018d92d12f941
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-10-17 14:31:35 +0300

    Time: 285 ms (26.71%), Space: 144.9 MB (9.33%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.cpp
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -1,63 +1,54 @@
 class Solution {
 public:
-    vector<string> find_neighbors(string node) {
-        // decrement(x) = (x - 1) % 10;
-        // increment(x) = (x + 1) % 10;
-        
-        // vector with all possible neighbor of node
-        vector<string> ans;
-
-        // each digit of the lock
-        for (int i = 0; i < 4; i++) {
-            int digit = node[i] - '0'; // convert string digit to int
-
-            // add and substract one from the digit
-            for (int change : {-1, 1}) {
-                // get the new digit
-                // int new_digit = (digit - '0' + change + 10) % 10 + '0';
-                int new_digit = (digit + change + 10) % 10;
-                
-                // compute the new node
-                string neigh = node;
-                neigh[i] = new_digit + '0';
-
-                ans.push_back(neigh);
-            }
-        }
-
-        return ans;
-    }
-
     int openLock(vector<string>& deadends, string target) {
-        // turn deadends into a set for O(1) check
-        set<string> seen;
+        // create a set with deadends for O(1) search
+        unordered_set<string> seen;
         for (string& s : deadends) {
             seen.insert(s);
         }
 
-        // start from '0000'
+        // check if start is in deadends
         if (seen.contains("0000")) return -1;
 
-        queue<pair<string, int>> queue;
+        queue<pair<string,int>> queue;     // {node, steps}
         queue.push({"0000", 0});
 
-        // start BFS from '0000'
         while (!queue.empty()) {
             auto [node, steps] = queue.front();
             queue.pop();
 
+            // check if we have reached the target
             if (node == target) return steps;
 
-            vector<string> neighs = find_neighbors(node);
-
-            for (string neighbor : neighs) {
-                if (!seen.contains(neighbor)) {
-                    seen.insert(neighbor);
-                    queue.push(pair(neighbor, steps + 1));
+            // get all possible neighbor of the current node
+            vector<string> neighs = findNeighbors(node);
+            for (string& neigh : neighs) {
+                if (!seen.contains(neigh)) {
+                    seen.insert(neigh);
+                    queue.push({neigh, steps + 1});
                 }
             }
         }
 
         return -1;
+    }
+
+    vector<string> findNeighbors(string node) {
+        vector<string> ans;
+
+        for (int i = 0; i < 4; i++) {
+            int curDigit = node[i] - '0';
+
+            for (int change : {-1, 1}) {
+                int newDigit = (curDigit + change + 10) % 10;
+
+                string neighbor = node;
+                neighbor[i] = newDigit + '0';
+
+                ans.push_back(neighbor);
+            }
+        }
+
+        return ans;
     }
 };
