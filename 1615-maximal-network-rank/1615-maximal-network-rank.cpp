f65987f744d45ee25022ba21bde84d26ed778ff0
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-10-20 16:35:36 +0300

    Time: 25 ms (53.58%), Space: 35.3 MB (75.41%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        // build the graph
         unordered_map<int, vector<int>> graph;
-        for (vector<int>& road : roads) {
+        for (auto& road : roads) {
             graph[road[0]].push_back(road[1]);
             graph[road[1]].push_back(road[0]);
         }
 
         int maxRank = 0;
-        for (int i = 0; i < n - 1; i++) {
+
+        for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                // get current rank for citites i and j
-                int currRank = graph[i].size() + graph[j].size();
-
-                // check if there is a road from city i to city j
-                for (int k = 0; k < graph[j].size(); k++) {
-                    if (graph[j][k] == i) {
-                        currRank--;
-                        break;
-                    }
+                int rank = graph[i].size() + graph[j].size();
+                // dacă există muchie directă între i și j, scade 1
+                if (find(graph[i].begin(), graph[i].end(), j) != graph[i].end()) {
+                    rank--;
                 }
-
-                maxRank = max(maxRank, currRank);
+                maxRank = max(maxRank, rank);
             }
         }
 
