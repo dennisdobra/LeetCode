4da66f863d9180701d12b46592f046e9af02844f
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-10-21 14:20:38 +0300

    Time: 55 ms (44.87%), Space: 51.2 MB (10.83%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
+    // int subarraySum(vector<int>& nums, int k) {
+    //     unordered_map<int, int> prefix_freq{{0, 1}};
+
+    //     int curr = 0;
+    //     int num_subarrays = 0;
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         curr += nums[i];
+
+    //         num_subarrays += prefix_freq[curr - k];
+
+    //         prefix_freq[curr]++;
+    //     }
+
+    //     return num_subarrays;
+    // }
+
     int subarraySum(vector<int>& nums, int k) {
-        vector<int> prefix = {nums[0]};
-        for (int i = 1; i < nums.size(); i++) {
-            prefix.push_back(nums[i] + prefix.back());
-        }
+        unordered_map<int, int> prefix_freq;
 
-        int answer = 0;
+        int curr = 0;
+        int num_subarrays = 0;
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i; j < nums.size(); j++) {
-                // check the sum of the subarray starting at index i and ending at index j
-                if (prefix[j] - prefix[i] + nums[i] == k) {
-                    answer++;
-                }
-            }
+            curr += nums[i];
+
+            // - ori varianta de sus unde initializez hasmap-ul cu intraream [0 -> 1]
+            //   pt a numara toate subarray-urile care incep de la indexul 0
+            // - ori mai pun un if unde verific doar subarray-urile care incep de la inceput
+            if (curr == k) num_subarrays++;
+
+            num_subarrays += prefix_freq[curr - k];
+
+            prefix_freq[curr]++;
         }
 
-        return answer;
+        return num_subarrays;
     }
 };
