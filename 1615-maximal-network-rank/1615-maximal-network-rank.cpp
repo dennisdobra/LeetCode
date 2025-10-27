8c774744b0b42d12bf84f2401c5c018b0c991f7d
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-10-27 13:22:27 +0200

    Time: 51 ms (23.3%), Space: 46.9 MB (11.7%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        unordered_map<int, unordered_set<int>> graph;
-        for (auto& road : roads) {
-            graph[road[0]].insert(road[1]);
-            graph[road[1]].insert(road[0]);
+        // city index -> list of directly connected cities
+        unordered_map<int, unordered_set<int>> directlyConnected;
+
+        for (vector<int> road : roads) {
+            int src = road[0], dst = road[1];
+
+            directlyConnected[src].insert(dst);
+            directlyConnected[dst].insert(src);
         }
 
-        int maxRank = 0;
-
-        for (int i = 0; i < n; i++) {
+        int maxRank = INT_MIN;
+        for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
-                int rank = graph[i].size() + graph[j].size();
-                // if there is an edge between city i and city j we only count it once
-                if (graph[i].contains(j)) {
-                    rank--;
-                }
-                maxRank = max(maxRank, rank);
+                int currRank = directlyConnected[i].size() + directlyConnected[j].size();
+
+                // if cities i and j are directly connected, only cound the road once
+                if (directlyConnected[i].contains(j)) currRank = currRank - 1;
+
+                if (currRank > maxRank) maxRank = currRank;
             }
         }
 
