85f71c0d0fd82e43368b30ffdc64931f8c804480
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-10-27 14:25:38 +0200

    Time: 339 ms (5.07%), Space: 163.2 MB (5.02%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int m;  // nr rows
+    int n;  // nr cols
+    vector<vector<int>> grid;
+    vector<vector<bool>> seen;
+
+    int islandPerimeter(vector<vector<int>>& grid) {
+        this->grid = grid;
+        m = grid.size();
+        n = grid[0].size();
+
+        int perimeter = 0;
+
+        int row = -1, col = -1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    row = i;
+                    col = j;
+                    cout << row << " " << col << endl;
+                    break;
+                }
+            }
+
+            if (row != -1) break;
+        }
+
+        // start a BFS from node [row][col]
+
+        queue<pair<int, int>> queue;
+        queue.push({row, col});
+
+        seen = vector(m, vector<bool>(n, false));
+        seen[row][col] = true;
+
+        while (!queue.empty()) {
+            auto [currRow, currCol] = queue.front();
+            queue.pop();
+
+            perimeter += isConnectedToWater(currRow, currCol);
+
+            // push the valid neighbours into the queue
+            for (vector<int> dir : directions) {
+                int newRow = currRow + dir[0];
+                int newCol = currCol + dir[1];
+
+                if (isValid(newRow, newCol) && grid[newRow][newCol] == 1 && seen[newRow][newCol] == false) {
+                    queue.push({newRow, newCol});
+                    seen[newRow][newCol] = true;
+                }
+            }
+        }
+
+        return perimeter;
+    }
+
+    bool isValid(int row, int col) {
+        return row >= 0 && row < m && col >= 0 && col < n;
+    }
+
+
+    int isConnectedToWater(int row, int col) {
+        int count = 0;
+        for (vector<int> dir : directions) {
+            if (isValid(row + dir[0], col + dir[1]) &&    // is a valid cell
+                grid[row + dir[0]][col + dir[1]] == 0)    // is water
+                count++;
+            
+            if (!isValid(row + dir[0], col + dir[1])) count++;
+        }
+            
+        return count;
+    }
+};
