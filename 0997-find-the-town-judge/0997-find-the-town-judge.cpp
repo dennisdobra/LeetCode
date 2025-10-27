e8847a9b463f009d3e3f408ee760f9ee9fb69f0d
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-10-27 13:11:58 +0200

    Time: 0 ms (100%), Space: 65.1 MB (66.91%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        unordered_map<int, vector<int>> trusts;
-        unordered_map<int, vector<int>> isTrusted;
+        vector<int> outdegree(n + 1, 0);
+        vector<int> indegree(n + 1, 0);
 
         for (vector<int>& relation : trust) {
-            int src = relation[0], dst = relation[1];
-
-            trusts[src].push_back(dst);
-            isTrusted[dst].push_back(src);
+            outdegree[relation[0]]++;
+            indegree[relation[1]]++;
         }
 
-        // find the person who trusts nobody
-        int trustsNobody = -1;
         for (int i = 1; i <= n; i++) {
-            if (!trusts.contains(i)) {
-                trustsNobody = i;
-                break;
+            if (indegree[i] == n - 1 && outdegree[i] == 0) {
+                return i;
             }
         }
-
-        if (isTrusted[trustsNobody].size() == n - 1) return trustsNobody;
 
         return -1;
     }
