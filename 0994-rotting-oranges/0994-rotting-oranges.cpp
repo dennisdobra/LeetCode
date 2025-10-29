6b6bb9ccd707f6687addfe579c9912ed54a8769a
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-10-29 22:10:49 +0200

    Time: 0 ms (100%), Space: 16.8 MB (60.1%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,82 +1,48 @@
 class Solution {
 public:
-    vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    vector<vector<int>> grid;
-    vector<vector<int>> times;
-    int m;
-    int n;
-
     int orangesRotting(vector<vector<int>>& grid) {
-        this->grid = grid;
-        m = grid.size();
-        n = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<pair<int, int>> q;
+        int fresh = 0;
 
-        times = vector(m, vector<int>(n, INT_MAX));
-
-        // start a BFS from every rotten orange
-        // queue storing (row, col, minutes)
-
-        queue<vector<int>> queue;
-
-        int freshOranges = 0;
-        int rottenOranges = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) {
-                    // found a rotten orange
-                    queue.push({i, j, 0});
-                    times[i][j] = 0;
-                    rottenOranges++;
-                } else if (grid[i][j] == 1) {
-                    freshOranges++;
-                }
+        // enqueue all rotten oranges
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 2)
+                    q.push({i, j});
+                else if (grid[i][j] == 1)
+                    ++fresh;
             }
         }
 
-        if (freshOranges == 0 && rottenOranges == 0) return 0;
+        if (fresh == 0) return 0;
 
-        if (rottenOranges == 0) return -1;
+        int minutes = 0;
+        vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-        int maxMinutes = 0;
-        while (!queue.empty()) {
-            vector<int> tmp = queue.front();
-            queue.pop();
+        // BFS level by level
+        while (!q.empty()) {
+            int size = q.size();
+            bool rotted = false;
 
-            int currRow = tmp[0], currCol = tmp[1], currMinutes = tmp[2];
+            for (int k = 0; k < size; ++k) {
+                auto [r, c] = q.front(); q.pop();
 
-            for (vector<int> dir : directions) {
-                int newRow = currRow + dir[0];
-                int newCol = currCol + dir[1];
-
-                if (isValid(newRow, newCol) && grid[newRow][newCol] != 0 &&
-                    currMinutes + 1 < times[newRow][newCol]) {
-                        // check if it is a fresh orange
-                        if (grid[newRow][newCol] == 1) freshOranges--;
-
-                        times[newRow][newCol] = currMinutes + 1;
-                        queue.push({newRow, newCol, currMinutes + 1});
-
-                        maxMinutes = max(maxMinutes, currMinutes + 1);
+                for (auto [dr, dc] : dirs) {
+                    int nr = r + dr, nc = c + dc;
+                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;    // mark as rotten
+                        q.push({nr, nc});
+                        rotted = true;
+                        --fresh;
+                    }
                 }
             }
+
+            if (rotted) ++minutes;
         }
 
-        if (freshOranges != 0) return -1;
-
-
-        // for (int i = 0; i < m; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         if (times[i][j] != INT_MAX && times[i][j] > maxMinutes) {
-        //             maxMinutes = times[i][j];
-        //         }
-        //     }
-        // }
-
-        return maxMinutes;
-    }
-
-    bool isValid(int row, int col) {
-        return row >= 0 && row < m && col >= 0 && col < n;
+        return fresh == 0 ? minutes : -1;
     }
 };
-
