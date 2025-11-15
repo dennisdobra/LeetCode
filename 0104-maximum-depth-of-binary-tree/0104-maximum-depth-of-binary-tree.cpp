041bf0897ccec588f6cfd81b8458d7ee2b4ea9b8
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-11-15 19:44:24 +0200

    Time: 0 ms (100%), Space: 19.1 MB (12.92%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -12,46 +12,11 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-
-        /* RECURSIVELY */
-        // if (root == nullptr) return 0;
-
-        // int left = maxDepth(root->left);
-        // int right = maxDepth(root->right);
-        // return 1 + max(left, right);
-
-        /* ITERATIVELY */
-        // if (root == nullptr) {
-        //     return 0;
-        // }
-
-        // stack<pair<TreeNode*, int>> stack;
-        // stack.push({root, 1});
-        // int ans = 0;
-
-        // while (!stack.empty()) {
-        //     auto [node, depth] = stack.top();
-        //     stack.pop();
-
-        //     ans = max(ans, depth);
-
-        //     if (node->left != nullptr) {
-        //         stack.push({node->left, depth + 1});
-        //     }
-
-        //     if (node->right != nullptr) {
-        //         stack.push({node->right, depth + 1});
-        //     }
-        // }
-
-        // return ans;
-
-
         if (root == nullptr) return 0;
 
-        int left_depth = maxDepth(root->left);
-        int right_depth = maxDepth(root->right);
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
 
-        return 1 + max(left_depth, right_depth);
+        return 1 + max(left, right);
     }
 };
