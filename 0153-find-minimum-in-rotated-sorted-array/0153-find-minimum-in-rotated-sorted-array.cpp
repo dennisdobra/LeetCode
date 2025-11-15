ecaeee67ca0854af155943a66e064bcfe662bb38
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-11-15 15:48:46 +0200

    Time: 0 ms (100%), Space: 14.3 MB (12.43%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,19 +1,47 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int l = 0;
-        int r = nums.size() - 1;
+        // If the list has just one element then return that element.
+        if (nums.size() == 1) {
+            return nums[0];
+        }
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
+        // Initializing left and right pointers.
+        int left = 0, right = nums.size() - 1;
 
-            if (nums[mid] > nums[nums.size() - 1]) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
+        // If the last element is greater than the first element then there is
+        // no rotation. E.g. 1 < 2 < 3 < 4 < 5 < 7. Already sorted array. Hence
+        // the smallest element is first element. A[0]
+        if (nums[left] < nums[right]) {
+            return nums[0];
+        }
+
+        while (right >= left) {
+            int mid = left + (right - left) / 2;
+
+            // If the mid element is greater than its next element then mid+1
+            // element is the smallest This point would be the point of change.
+            // From higher to lower value.
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+
+            // If the mid element is lesser than its previous element then mid
+            // element is the smallest
+            if (nums[mid] < nums[mid - 1]) {
+                return nums[mid];
+            }
+
+            // If the mid elements value is greater than the 0th element this
+            // means the least value is still somewhere to the right as we are
+            // still dealing with elements greater than nums[0]
+            if (nums[mid] > nums[0]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[0]) {
+                right = mid - 1;
             }
         }
 
-        return nums[l];
+        return -1;
     }
 };
