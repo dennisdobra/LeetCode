851adba194c65a02fe9e0a588d502d2afac57790
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-11-15 14:42:55 +0200

    Time: 5 ms (9.13%), Space: 97.2 MB (86.96%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        /* Complexitate temp: O(n^2) */
-        
-        // int buy, sell, maxProfit = 0;
-        // for (int i = 0; i < prices.size() - 1; i++) {
-        //     for (int j = i + 1; j < prices.size(); j++) {
-        //         if (prices[j] - prices[i] > maxProfit) {
-        //             maxProfit = prices[j] - prices[i];
-        //             buy = i;
-        //             sell = j;
-        //         }
-        //     }
-        // }
-
         int minPrice = INT_MAX;
         int maxProfit = 0;
 
