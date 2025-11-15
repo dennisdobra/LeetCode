5fdfe467566cc43cde41de65d002fbfa2489669c
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-11-15 17:20:35 +0200

    Time: 2 ms (15.48%), Space: 9 MB (38.01%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,35 +3,25 @@ public:
     bool isValid(string s) {
         stack<char> stack;
 
-        for (char c : s) {
-            if (stack.empty()) {
-                if (c == '(' || c == '{' || c == '[') {
-                    stack.push(c);
-                    continue;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                stack.push(s[i]);
+            } else {
+                if (stack.empty()) return false;
+
+                if (s[i] == ')') {
+                    if (stack.top() != '(') return false;
+                    stack.pop();
+                } else if (s[i] == ']') {
+                    if (stack.top() != '[') return false;
+                    stack.pop();
+                } else if (s[i] == '}') {
+                    if (stack.top() != '{') return false;
+                    stack.pop();
                 }
-                return false;
             }
-
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-                continue;
-            }
-
-            if (c == ')') {
-                if (stack.top() != '(') return false;
-            }
-
-            if (c == ']') {
-                if (stack.top() != '[') return false;
-            }
-
-            if (c == '}') {
-                if (stack.top() != '{') return false;
-            }
-
-            stack.pop();
         }
 
-        return stack.empty() ? true : false;
+        return stack.empty();
     }
 };
