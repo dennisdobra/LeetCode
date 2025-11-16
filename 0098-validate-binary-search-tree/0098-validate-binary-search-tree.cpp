922a46870ee6f123296d0ff4ee41619bf428350a
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-11-16 12:35:17 +0200

    Time: 0 ms (100%), Space: 22 MB (18.77%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -21,9 +21,12 @@ public:
     bool dfs(TreeNode* node, long small, long large) {
         if (node == nullptr) return true;
 
-        // check constraint for currernt node
+        // check constraint for current node
         if (node->val <= small || node->val >= large) return false;
 
-        return dfs(node->left, small, node->val) && dfs(node->right, node->val, large);
+        bool left = dfs(node->left, small, node->val);
+        bool right = dfs(node->right, node->val, large);
+
+        return left && right;
     }
 };
