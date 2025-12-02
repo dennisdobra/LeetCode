149f1d221d3cdbe94d358580facaf1b72d2bc744
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-12-02 18:54:51 +0200

    Time: 33 ms (92.26%), Space: 45.4 MB (69.5%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-    // int subarraySum(vector<int>& nums, int k) {
-    //     unordered_map<int, int> prefix_freq{{0, 1}};
-
-    //     int curr = 0;
-    //     int num_subarrays = 0;
-    //     for (int i = 0; i < nums.size(); i++) {
-    //         curr += nums[i];
-
-    //         num_subarrays += prefix_freq[curr - k];
-
-    //         prefix_freq[curr]++;
-    //     }
-
-    //     return num_subarrays;
-    // }
-
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> prefix_freq;
+        unordered_map<int,int> prefixFreq;
+
+        // we don t want to miss a prefix subarray with a sum equal to k
+        prefixFreq[0] = 1;  
 
         int curr = 0;
-        int num_subarrays = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            curr += nums[i];
+        int ans = 0;
+        for (int num : nums) {
+            curr += num;
 
-            // - ori varianta de sus unde initializez hasmap-ul cu intraream [0 -> 1]
-            //   pt a numara toate subarray-urile care incep de la indexul 0
-            // - ori mai pun un if unde verific doar subarray-urile care incep de la inceput
-            if (curr == k) num_subarrays++;
+            if (prefixFreq.contains(curr - k)) {
+                ans += prefixFreq[curr - k];
+            }
 
-            num_subarrays += prefix_freq[curr - k];
-
-            prefix_freq[curr]++;
+            prefixFreq[curr]++;
         }
 
-        return num_subarrays;
+        return ans;
     }
 };
