827ba1e3ecba84e102ca9f9af5dd24ce342075d1
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-12-03 13:51:24 +0200

    Time: 92 ms (19.9%), Space: 48.1 MB (24.08%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        unordered_map<string,int> row_freq;
-        unordered_map<string,int> col_freq;
+        unordered_map<string,int> rowsFreq;
+        for (int i = 0; i < grid.size(); i++) {
+            string row = "";
+            for (int j = 0; j < grid[0].size(); j++) {
+                row += to_string(grid[i][j]);
+                row += ',';
+            }
+            row.pop_back();
+
+            rowsFreq[row]++;
+        }
 
         int ans = 0;
-
-        // iterate over each row
-        for (int i = 0; i < grid.size(); i++) {
-            row_freq[hashFunc(grid[i])]++;
-        }
-
-        // iterate over each col
-        for (int i = 0; i < grid.size(); i++) {
-            vector<int> curr_col;
-            for (int j = 0; j < grid.size(); j++) {
-                curr_col.push_back(grid[j][i]);
+        for (int j = 0; j < grid[0].size(); j++) {
+            string col = "";
+            for (int i = 0; i < grid.size(); i++) {
+                col += to_string(grid[i][j]);
+                col += ',';
             }
+            col.pop_back();
 
-            col_freq[hashFunc(curr_col)]++;
-        }
-
-        // iterate over the 2 hash maps
-        for (auto& pair : row_freq) {
-            if (col_freq.contains(pair.first)) {
-                ans += (pair.second * col_freq[pair.first]);
+            if (rowsFreq.contains(col)) {
+                ans += rowsFreq[col];
             }
         }
 
         return ans;
-    }
-
-    string hashFunc(vector<int> nums) {
-        string s = "";
-
-        for (int i = 0; i < nums.size(); i++) {
-            s += to_string(nums[i]) + ',';
-        }
-
-        return s;
     }
 };
