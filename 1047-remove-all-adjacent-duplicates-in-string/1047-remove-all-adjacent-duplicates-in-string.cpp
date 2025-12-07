c1a12f9e45f28b6b4a0f0074f10f8f52aa0d5937
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-12-08 00:43:32 +0200

    Time: 9 ms (32.66%), Space: 14.1 MB (44.31%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,31 +2,30 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         stack<char> stack;
-        stack.push(s[0]);
 
-        for (int i = 1; i < s.size(); i++) {
-            char curr = s[i];
+        bool skip = false;
+        for (char c : s) {
+            while (!stack.empty() && stack.top() == c) {
+                stack.pop();
+                skip = true;
+            }
 
-            if (stack.empty() || (!stack.empty() && curr != stack.top())) {
-                stack.push(curr);
+            if (skip == true) {
+                skip = false;
                 continue;
             }
 
-            while (!stack.empty() && curr == stack.top()) {
-                stack.pop();
-            }
+            stack.push(c);
         }
 
-        string ans = "";
-        while(!stack.empty()) {
-            char curr = stack.top();
+        string reversed = "";
+        while (!stack.empty()) {
+            reversed += stack.top();
             stack.pop();
-
-            ans += curr;
         }
 
-        reverse(ans.begin(), ans.end());
+        reverse(reversed.begin(), reversed.end());
 
-        return ans;
+        return reversed;
     }
 };
