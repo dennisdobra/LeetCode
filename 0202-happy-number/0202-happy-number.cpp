22e32ac9e40d64ea5561a5ed43aad19578462a00
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-12-08 23:57:58 +0200

    Time: 2 ms (26.34%), Space: 8.2 MB (53.36%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,31 +2,22 @@ class Solution {
 public:
     bool isHappy(int n) {
         // use an unordered set to check for infinite loops
-        unordered_set<int> set;
+        unordered_set<int> seen;
 
-        int sum = 0;
-        while (1) {
-            sum = squareDigits(n);
-            if (sum == 1) return true;
-
-            if (set.contains(sum)) return false;
-
-            set.insert(sum);
-            n = sum;
+        while (n != 1 && !seen.contains(n)) {
+            seen.insert(n);
+            n = squareDigits(n);
         }
 
-        return false;
+        return n == 1;
     }
 
     int squareDigits(int n) {
         int sum = 0;
-
         while (n != 0) {
             sum += pow(n % 10, 2);
             n = n / 10;
         }
-
-        cout << sum << endl;
 
         return sum;
     }
