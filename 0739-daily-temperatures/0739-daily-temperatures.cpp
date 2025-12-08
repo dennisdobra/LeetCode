404a133052f87ca02711ef3ab2c36a21c24f4f2e
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-12-08 16:06:24 +0200

    Time: 26 ms (40.89%), Space: 107.3 MB (41.72%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         stack<int> monotonic;
-
         vector<int> ans(temperatures.size());
 
         for (int i = 0; i < temperatures.size(); i++) {
             while (!monotonic.empty() && temperatures[i] > temperatures[monotonic.top()]) {
-                // found a warmer temperatures than the one at the top of the stack
-                int idx = monotonic.top();
-                ans[idx] = i - idx;
+                // we found a warmer day
+                ans[monotonic.top()] = i - monotonic.top();
                 monotonic.pop();
             }
 
