77e411fc4bfbf4eb354d72a3380f9fb90b9fbaac
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-12-08 22:47:21 +0200

    Time: 0 ms (100%), Space: 12.5 MB (7.41%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if (strs.size() == 1) {
-            return strs[0];
-        }
-
         string prefix = strs[0];
 
-        for (int i = 1; i < strs.size(); i++) {
-            prefix = lcp(prefix, strs[i]);
+        for (string s: strs) {
+            prefix = lcp(prefix, s);
         }
 
         return prefix;
     }
 
-private:
-    string lcp(const string a, const string b) {
-        int minSize = 205;
-        if (a.size() < b.size()) {
-            minSize = a.size();
-        } else {
-            minSize = b.size();
-        }
+    string lcp(string s, string t) {
+        int size = min(s.size(), t.size());
 
-        int i;
         string lcp = "";
-        for (i = 0; i < minSize; i++) {
-            if (a[i] == b[i]) {
-                lcp += a[i];
-            } else {
-                break;
-            }
+        for (int i = 0; i < size; i++) {
+            if (s[i] != t[i]) return lcp;
+
+            lcp += s[i];
         }
 
         return lcp;
