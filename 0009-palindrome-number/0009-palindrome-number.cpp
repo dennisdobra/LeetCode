969ed42eed4e70a07ad30d19b7e7a2ddaceca023
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2025-12-08 22:35:31 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string val = to_string(x);
+        /* APPROACH 1: CONVERT TO STRING */
+        // string val = to_string(x);
 
-        int left = 0;
-        int right = val.size() - 1;
+        // int left = 0;
+        // int right = val.size() - 1;
 
-        while (left <= right) {
-            if (val[left] != val[right]) return false;
+        // while (left <= right) {
+        //     if (val[left] != val[right]) return false;
 
-            left++;
-            right--;
+        //     left++;
+        //     right--;
+        // }
+
+        // return true;
+
+        /* APPROACH 2: NO CONVERSION TO STRING */
+        if (x < 0) return false;
+
+        int reverse = 0;
+        int copy = x;
+
+        while (copy != 0) {
+            reverse = reverse * 10 + (copy % 10);
+            copy = copy / 10;
         }
 
-        return true;
+        return reverse == x;
     }
 };
