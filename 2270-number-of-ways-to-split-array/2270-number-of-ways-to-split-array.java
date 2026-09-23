e047029fe6a9c94d58e96bcae8814d5e31619dea
Dobra Dennis-Stefan <dennisdobra@gmail.com>
2026-09-23 14:40:29 +0300

    Time: 2 ms (100%), Space: 102.1 MB (46.4%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int waysToSplitArray(int[] nums) {
+    public int waysToSplitArray_(int[] nums) {
         long prefix[] = new long[nums.length];
 
         prefix[0] = nums[0];
@@ -11,6 +11,26 @@ class Solution {
 
         for (int i = 0; i < nums.length - 1; i++) {
             if (prefix[i] >= prefix[prefix.length - 1] - prefix[i]) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+
+    public int waysToSplitArray(int[] nums) {
+        long totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int ans = 0;
+
+        long left = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            left += nums[i];
+
+            if (left >= totalSum - left) {
                 ans++;
             }
         }
